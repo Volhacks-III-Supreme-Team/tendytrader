dc243fa58ff152f8d0e21dcbebee9691572c919c
ilumsden <lumsden.ian@gmail.com>
2018-09-30 00:09:23 -0400

    Finishes the average_sentiment (or whatever it is) function.

--- tendytrader/reddit/sentiment_analysis.py
+++ tendytrader/reddit/sentiment_analysis.py
@@ -7,6 +7,7 @@ def average_sentiment(term, **kwargs):
     # There is also polarity
     sum_polarity = 0.0
     sum_sentiment = 0.0
+    num = 0
     abs_in = os.path.abspath('../data/reddit/')
     pd = read_csv(os.path.join(abs_in, term + '.csv'))
     if 'subreddit' in kwargs:
@@ -23,9 +24,12 @@ def average_sentiment(term, **kwargs):
                 if i >= kwargs['num_posts']-1:
                     break
                 sum_polarity, sum_sentiment += st.sentiment
+                num += 1
         else:
             for sentence in text.sentences:
                 st = TextBlob(sentence)
                 sum_polarity, sum_sentiment += st.sentiment
-
-            
+                num += 1
+    avg_polarity = sum_polarity / num
+    avg_sentiment = sum_sentiment / num
+    return avg_polarity, avg_sentiment
